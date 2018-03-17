2fe2e6dfc9393216dcee924eb81cd5ee1215d4fe
Dave Powers <djpowers89@gmail.com>
2018-03-17 14:17:10 -0400

    Add ability to select team from dropdown menu (redraws entire svg)

--- client/src/app.js
+++ client/src/app.js
@@ -8,53 +8,111 @@ const margin = {
 const width = 600 - margin.left - margin.right;
 const height = 400 - margin.top - margin.bottom;
 
-d3
-  .request(`/api/seasons/${new Date().getFullYear() - 1}/bos`)
-  .get((error, request) => {
-    const data = JSON.parse(request.response).teamgamelogs.gamelogs;
-    console.log(data);
+const teams = [
+  'LAD',
+  'CLE',
+  'HOU',
+  'WAS',
+  'BOS',
+  'ARI',
+  'CHC',
+  'NYY',
+  'COL',
+  'MIL',
+  'MIN',
+  'STL',
+  'TB',
+  'KC',
+  'LAA',
+  'TEX',
+  'SEA',
+  'MIA',
+  'TOR',
+  'BAL',
+  'PIT',
+  'OAK',
+  'ATL',
+  'SD',
+  'NYM',
+  'CIN',
+  'CWS',
+  'PHI',
+  'DET',
+  'SF',
+];
 
-    const extentX = d3.extent(data, d => new Date(d.game.date));
-    const extentY = d3.extent(data, d =>
-      parseInt(d.stats.RunDifferential['#text']));
+const dropDown = d3
+  .select('body')
+  .append('select')
+  .attr('name', 'team-list');
 
-    const svg = d3
-      .select('body')
-      .append('svg')
-      .attr('width', width + margin.left + margin.right)
-      .attr('height', height + margin.top + margin.bottom)
-      .append('g')
-      .attr('transform', `translate(${margin.left}, ${margin.top})`);
+const options = dropDown
+  .selectAll('option')
+  .data(teams)
+  .enter()
+  .append('option');
 
-    const yScale = d3
-      .scaleLinear()
-      .domain(extentY)
-      .range([height, 0]);
+options.text(d => d).attr('value', d => d);
 
-    svg.call(d3.axisLeft(yScale));
+updateChart();
 
-    const xScale = d3
-      .scaleBand()
-      .domain(data.map(d => d.game.date))
-      .range([0, width]);
+function updateChart() {
+  d3
+    .request(`/api/seasons/${new Date().getFullYear() - 1}/${
+      d3.select('[name=team-list]').node().value
+    }`)
+    .get((error, request) => {
+      const data = JSON.parse(request.response).teamgamelogs.gamelogs;
+      console.log(data);
 
-    svg
-      .append('g')
-      .attr('transform', `translate(0, ${height})`)
-      .call(d3.axisBottom(xScale));
+      const extentX = d3.extent(data, d => new Date(d.game.date));
+      const extentY = d3.extent(data, d =>
+        parseInt(d.stats.RunDifferential['#text']));
 
-    svg
-      .selectAll('rect')
-      .data(data)
-      .enter()
-      .append('rect')
-      .attr('x', d => xScale(d.game.date))
-      .attr('y', d => yScale(parseInt(d.stats.RunDifferential['#text'])))
-      .attr('width', d => xScale.bandwidth())
-      .attr(
-        'height',
-        d => height - yScale(parseInt(d.stats.RunDifferential['#text'])),
-      )
-      .classed('win', d => parseInt(d.stats.Wins['#text']))
-      .classed('loss', d => parseInt(d.stats.Wins['#text']) === 0);
-  });
+      const svg = d3
+        .select('body')
+        .append('svg')
+        .attr('width', width + margin.left + margin.right)
+        .attr('height', height + margin.top + margin.bottom)
+        .append('g')
+        .attr('transform', `translate(${margin.left}, ${margin.top})`);
+
+      const yScale = d3
+        .scaleLinear()
+        .domain(extentY)
+        .range([height, 0]);
+
+      svg.call(d3.axisLeft(yScale));
+
+      const xScale = d3
+        .scaleBand()
+        .domain(data.map(d => d.game.date))
+        .range([0, width]);
+
+      svg
+        .append('g')
+        .attr('transform', `translate(0, ${height})`)
+        // .attr('transform', `translate(0, ${height / 2})`)
+        .call(d3.axisBottom(xScale));
+
+      svg
+        .selectAll('rect')
+        .data(data)
+        .enter()
+        .append('rect')
+        .attr('x', d => xScale(d.game.date))
+        .attr('y', d => yScale(parseInt(d.stats.RunDifferential['#text'])))
+        .attr('width', d => xScale.bandwidth())
+        .attr(
+          'height',
+          d => height - yScale(parseInt(d.stats.RunDifferential['#text'])),
+        )
+        .classed('win', d => parseInt(d.stats.Wins['#text']))
+        .classed('loss', d => parseInt(d.stats.Wins['#text']) === 0);
+    });
+}
+
+d3.select('[name=team-list]').on('change', () => {
+  d3.select('svg').remove();
+  updateChart();
+});
